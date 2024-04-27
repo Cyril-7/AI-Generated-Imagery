69db99b0cc1ce181cd763db9831afe9cf3366f55
Cyril T Johnson <129573220+Cyril-7@users.noreply.github.com>
2024-04-27 21:32:31 +0530

    ai_image.py

--- ai_image.py
+++ ai_image.py
@@ -2,34 +2,6 @@ from pathlib import Path
 import torch
 from diffusers import StableDiffusionPipeline
 
-# class CFG:
-#     device = "cuda"
-#     seed = 42
-#     generator = torch.Generator(device).manual_seed(seed)
-#     image_gen_steps = 35
-#     image_gen_model_id = "stabilityai/stable-diffusion-2"
-#     image_gen_size = (400,400)
-#     image_gen_guidance_scale = 9
-     
-# image_gen_model = StableDiffusionPipeline.from_pretrained(
-#     CFG.image_gen_model_id, torch_dtype=torch.float16,
-#     revision="fp16", use_auth_token='your_hugging_face_auth_token', guidance_scale=9
-# )
-# image_gen_model = image_gen_model.to(CFG.device)
-
-# def generate_image(prompt, model):
-#     image = model(
-#         prompt, num_inference_steps=CFG.image_gen_steps,
-#         generator=CFG.generator,
-#         guidance_scale=CFG.image_gen_guidance_scale
-#     ).images[0]
-    
-#     image = image.resize(CFG.image_gen_size)
-#     return image
-
-
-
-
 class CFG:
     device = "cuda"
     seed = 42
